ef039ce4f61a419bf24fcfd4e22b7868a4f5e8c9
Yeizer Marrugo <yeizermarrugo@gmail.com>
2022-09-15 19:53:22 -0500

    modify database

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,13 +1,26 @@
 const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
 
-const  db = new Sequelize({
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: '1234',
-    database: 'Airbnb',
-    port: 5432
-})
+dotenv.config({ path: './config.env' });
+
+// Create a connection to database
+const db = new Sequelize({ 
+  dialect: 'postgres', 
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
+  logging: false,
+  dialectOptions:
+    process.env.NODE_ENV === 'production'
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
+});
 
 module.exports = {
     db
